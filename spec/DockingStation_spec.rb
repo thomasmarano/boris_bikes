ab62f699b3b485b9276ecece5ad06e6d646ba804
jesslns <jesslns@yahoo.com.hk>
2019-01-09 14:14:08 +0000

    Make Docking Station gets bike

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,4 +1,10 @@
 require "docking_station"
 describe DockingStation do
   it { is_expected.to respond_to :release_bike}
+
+  it "release a bike" do
+    #station = DockingStation.new
+    bike = subject.release_bike
+    expect(bike.working?).to eq true
+  end
 end
