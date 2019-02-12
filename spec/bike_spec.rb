e55ae494c39ce64e3b92c14de9754063961d0d5f
thomasmarano <thomas.marano2@gmail.com>
2019-02-12 13:51:12 +0000

    Committed All

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,7 +3,7 @@ describe Bike do
     it { is_expected.to respond_to :working?}
 
     it 'reports bike as broken' do
-      subject.report_broken
+      subject.report_boken
       expect(subject.broken?).to eq true
     end
 end
